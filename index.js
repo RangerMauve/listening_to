eeb91ffa0d975bab15d31d8a719044fdd8b60da7
RangerMauve <RangerMauve@hotmail.com>
2014-06-17 20:11:08 -0400

    Broadcast when user changes their song

--- index.js
+++ index.js
@@ -28,11 +28,12 @@ listenerServer.on("connection", function (listener) {
 	listener.once("data", function (data) {
 		data = JSON.parse(data); // Parse data as JSON
 		id = data; // Set the id to their data
-		console.log(id,"connected");
+		console.log(id, "connected");
 		// Register their data in the listeners map
 		listeners[id] = {
 			id: id,
-			updated: Date.now()
+			updated: Date.now(),
+			socket: listener
 		};
 		// Set up actual data listener
 		listener.on("data", function (data) {
@@ -46,21 +47,27 @@ listenerServer.on("connection", function (listener) {
 	listener.on("song", function (data) {
 		// Event for changing the currently lsitened-to song
 		var song = data.song;
-		console.log(id,"now listening to",song);
+		console.log(id, "now listening to", song);
 		// Update the user's current song
 		listeners[id].song = song;
 		// Update timestamp for sorting
 		listeners[id].updated = Date.now();
+		// Whenever you change your song, broadcast the change
+		broadcast({
+			type: "change_song",
+			song: song,
+			id: id
+		});
 	});
 	listener.on("stopped", function (data) {
 		// Event for when user stops listening to anything
-		console.log(id,"stopped listening");
+		console.log(id, "stopped listening");
 		listeners[id].song = "";
 		listeners[id].updated = Date.now();
 	});
 	listener.on("close", function () {
 		// Called when the websocket is closed
-		console.log(id,"disconnected");
+		console.log(id, "disconnected");
 		// Delete the listener's data from the map
 		delete listeners[id];
 	});
@@ -79,19 +86,19 @@ app.get("/songs", function (req, res) {
 app.get("/listening", function (req, res) {
 	// Get a list of all the ids of the listeners
 	var songs = Object.keys(listeners)
-	.map(function(id){ // Map an array with the listener data
-		return listeners[id];
-	}).filter(function (listener) { // Filter out listeners that aren't listening to anything
-		return !!listener.song;
-	}).sort(function (plistener, listener) { // Sort listeners by most recently updated
-		return plistener.updated < listener.updated;
-	}).map(function (listener) { // Get onlt their id and song
-		return {
-			id: listener.id,
-			song: listener.song
-		};
-	});
-	console.log("User getting listener list",songs,"from",listeners);
+		.map(function (id) { // Map an array with the listener data
+			return listeners[id];
+		}).filter(function (listener) { // Filter out listeners that aren't listening to anything
+			return !!listener.song;
+		}).sort(function (plistener, listener) { // Sort listeners by most recently updated
+			return plistener.updated < listener.updated;
+		}).map(function (listener) { // Get onlt their id and song
+			return {
+				id: listener.id,
+				song: listener.song
+			};
+		});
+	console.log("User getting listener list", songs, "from", listeners);
 	res.json(songs); // Return the data as a JSON array
 });
 
@@ -111,6 +118,15 @@ listenerServer.installHandlers(server, {
 
 // Listen to PORT provided by Heroku or default (80)
 server.listen(process.env.PORT || 80);
+
+function broadcast(data) {
+	Object.keys(listeners).map(function (id) {
+		return listeners[id];
+	}).forEach(function (listener) {
+		var sock = listener.socket;
+		sock.write(JSON.stringify(data));
+	});
+}
 
 // Returns a promise that resolves to the list of songs `in static/songs/`
 function getSongs() {
